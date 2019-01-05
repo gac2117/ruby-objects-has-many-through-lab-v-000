656386a1524f2abd398a5f55874a82710fbfe0e0
Grace Choi <oiograceoio@gmail.com>
2019-01-05 09:01:19 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,5 +1,5 @@
-class Patient 
-  attr_accessor :name 
+class Patient
+  attr_accessor :name
   @@all = []
 
   def initialize(name)
@@ -7,8 +7,8 @@ class Patient
     @@all << self
   end
 
-  def self.all 
-    @@all 
+  def self.all
+    @@all
   end
 
   def new_appointment(date, doctor)
@@ -23,7 +23,7 @@ class Patient
 
   def doctors
     appointments.map do |app|
-      app.doctor 
+      app.doctor
     end
   end
 end
