5315b7726261229192aa0632ce03b1d578c2a0d0
Grace Choi <oiograceoio@gmail.com>
2019-01-05 08:56:13 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,29 @@
+class Doctor
+  attr_accessor :name, :date, :appointment, :patient 
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all 
+    @@all 
+  end
+
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end
+
+  def appointments
+    Appointment.all.select do |app|
+      app.doctor == self
+    end
+  end
+
+  def patients
+    appointments.map do |app|
+      app.patient
+    end
+  end
+end
